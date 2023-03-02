033266745e7566ef6d1902c290bbc624a6edad6b
Michael Smitte <smittemichael@yahoo.com>
2023-03-02 16:53:52 -0500

    initialized application and copies CSS structure

---
+++ fab-game/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <h1>
+          Flesh and Blood Card Guesser!
+        </h1>
+      </nav>
+
+    </div>
+  );
+}
+
+export default App;
